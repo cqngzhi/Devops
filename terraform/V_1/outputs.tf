7d06cad7cb24abc8eb265496684322b74cda1acc
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 19:50:01 +0100

    Update outputs.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/outputs.tf
+++ terraform/V_1/outputs.tf
@@ -7,3 +7,13 @@ output "private_key_path" {
   value       = local_file.private_key.filename
   description = "Path to the locally stored private key."
 }
+
+output "cloudflare_zone_id" {
+  value       = cloudflare_zone.example_zone.id
+  description = "The Cloudflare zone ID for the domain."
+}
+
+output "openvpn_dns_record" {
+  value       = cloudflare_record.openvpn_dns.hostname
+  description = "The DNS record for the OpenVPN instance."
+}
