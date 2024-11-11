268f55e378e57d8ab627806092d26e199f887c0e
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 19:16:00 +0100

    Create variables.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

---
+++ terraform/V_1/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_session_token" {
+  description = "AWS session token"
+  type        = string
+  sensitive   = true
+}
+
+variable "counter" {
+  default     = 1
+  description = "Number of instances to create"
+}
