32342931a1a15a83027cec6d3100d7e06f78a4f8
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 20:21:05 +0100

    Update cloudflare.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/cloudflare.tf
+++ terraform/V_1/cloudflare.tf
@@ -1,14 +1,14 @@
 # Cloudflare zone
 resource "cloudflare_zone" "example_zone" {
-  zone = "example.com"  # 您的域名
+  zone = "example.com"  # Uw domeinnaam
 }
 
-# Cloudflare DNS记录
+# Cloudflare DNS record
 resource "cloudflare_record" "openvpn_dns" {
   zone_id = cloudflare_zone.example_zone.id
   name    = "openvpn"
   type    = "A"
-  value   = aws_eip.openvpn_ip[0].public_ip  # 使用第一台实例的弹性IP
+  value   = aws_eip.openvpn_ip[0].public_ip  # Het publieke IP van de eerste EC2 instantie (Elastic IP)
   ttl     = 3600
-  proxied = false  # 如果想使用Cloudflare代理功能可设为true
+  proxied = true  # Als u Cloudflare's proxy-functionaliteit wilt gebruiken, zet dit op true
 }
