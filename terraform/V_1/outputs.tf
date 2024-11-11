5dfab7f2c2582e49cd55b23bff6f9ea0c8ca69f8
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 20:27:41 +0100

    Update outputs.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/outputs.tf
+++ terraform/V_1/outputs.tf
@@ -1,18 +1,22 @@
+# Toon de publieke IP-adressen van de OpenVPN EC2-instanties
 output "instance_public_ips" {
   value       = [for ip in aws_eip.openvpn_ip : ip.public_ip]
   description = "Public IP addresses of OpenVPN EC2 instances."
 }
 
+# Toon het pad naar de lokaal opgeslagen privésleutel
 output "private_key_path" {
   value       = local_file.private_key.filename
   description = "Path to the locally stored private key."
 }
 
+# Toon de Cloudflare zone ID voor het domein
 output "cloudflare_zone_id" {
   value       = cloudflare_zone.example_zone.id
   description = "The Cloudflare zone ID for the domain."
 }
 
+# Toon het DNS-record voor de OpenVPN-instantie
 output "openvpn_dns_record" {
   value       = cloudflare_record.openvpn_dns.hostname
   description = "The DNS record for the OpenVPN instance."
