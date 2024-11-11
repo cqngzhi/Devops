9e1c024f302e0fab30c1d9b801fc5c770a3026b4
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 20:35:18 +0100

    Update variables.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/variables.tf
+++ terraform/V_1/variables.tf
@@ -1,32 +1,38 @@
+# Variabele voor de AWS toegangssleutel
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
   sensitive   = true
 }
 
+# Variabele voor de AWS geheime sleutel
 variable "aws_secret_key" {
   description = "AWS secret key"
   type        = string
   sensitive   = true
 }
 
+# Variabele voor het AWS sessie-token
 variable "aws_session_token" {
   description = "AWS session token"
   type        = string
   sensitive   = true
 }
 
+# Variabele voor het aantal te maken instanties
 variable "counter" {
   default     = 1
   description = "Number of instances to create"
 }
 
+# Variabele voor het e-mailadres van het Cloudflare-account
 variable "cloudflare_email" {
   description = "Cloudflare account email"
   type        = string
   sensitive   = true
 }
 
+# Variabele voor de Cloudflare API-token
 variable "cloudflare_api_token" {
   description = "Cloudflare API token"
   type        = string
