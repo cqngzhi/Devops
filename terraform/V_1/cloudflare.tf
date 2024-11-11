71a9701734df86f4ef7d82c5f23018a8a0954293
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 23:09:54 +0100

    Update cloudflare.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/cloudflare.tf
+++ terraform/V_1/cloudflare.tf
@@ -3,7 +3,7 @@ resource "cloudflare_record" "openvpn_dns" {
   zone_id = "cb06796a450b34dd4cb1d2b307706ecd"
   name    = "openvpn"
   type    = "A"
-  value   = aws_eip.openvpn_ip[0].public_ip  # Het publieke IP van de eerste EC2 instantie (Elastic IP)
-  ttl     = 3600
+  content = aws_eip.openvpn_ip[0].public_ip  # Het publieke IP van de eerste EC2 instantie (Elastic IP)
+  ttl     = 1
   proxied = true  # Als u Cloudflare's proxy-functionaliteit wilt gebruiken, zet dit op true
 }
