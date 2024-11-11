779a0e8fdb4cf21183ae876a19f8842a0562385b
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 19:46:33 +0100

    Update providers.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/providers.tf
+++ terraform/V_1/providers.tf
@@ -7,10 +7,21 @@ terraform {
   }
   required_version = ">= 1.0"
 }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
 
 provider "aws" {
   region     = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   token      = var.aws_session_token
+}
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_token = var.cloudflare_api_token
 }
