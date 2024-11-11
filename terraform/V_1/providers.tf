19c66e6b9b0597d592e4451be30358c383036b02
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 20:30:32 +0100

    Update providers.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/providers.tf
+++ terraform/V_1/providers.tf
@@ -1,5 +1,7 @@
+# Geef de vereiste Terraform versies en providers aan
 terraform {
   required_providers {
+    # AWS provider configuratie
     aws = {
       source = "hashicorp/aws"
       version = "~> 5.0"
@@ -7,6 +9,7 @@ terraform {
   }
   required_version = ">= 1.0"
 }
+ # Cloudflare provider configuratie
     cloudflare = {
       source = "cloudflare/cloudflare"
       version = "~> 4.0"
@@ -15,12 +18,15 @@ terraform {
   required_version = ">= 1.0"
 }
 
+# Configureer de AWS provider
 provider "aws" {
   region     = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   token      = var.aws_session_token
 }
+
+# Configureer de Cloudflare provider
 provider "cloudflare" {
   email   = var.cloudflare_email
   api_token = var.cloudflare_api_token
