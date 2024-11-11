1057c5862dcaf913a2feef57a01668f60d759126
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-11 19:17:10 +0100

    Create security.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

---
+++ terraform/V_1/security.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "security" {
+  name_prefix = "openvpn_security_group"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow OpenVPN"
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
