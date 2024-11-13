ae1e7538b96a751014860e5a969f97c891782fcd
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-13 22:37:35 +0100

    Update generate_server_yml.sh
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/generate_server_yml.sh
+++ terraform/V_1/generate_server_yml.sh
@@ -10,16 +10,8 @@ ssh_key_path=$(jq -r '.private_key_path.value' terraform_outputs.json)
 # Stel de gebruikersnaam in op basis van het AMI-besturingssysteem
 ami_name=$(jq -r '.ami_name.value[0]' terraform_outputs.json)
 
-# Hier stellen we de gebruiker in voor verschillende besturingssystemen
-if [[ "$ami_name" == *"ubuntu"* ]]; then
+# ansible user is ubuntu
   ansible_user="ubuntu"
-elif [[ "$ami_name" == *"amzn"* || "$ami_name" == *"amazon"* ]]; then
-  ansible_user="ec2-user"
-elif [[ "$ami_name" == *"centos"* ]]; then
-  ansible_user="centos"
-else
-  ansible_user="admin"
-fi
 
 # Maak het inventory.yaml-bestand en schrijf het naar de opgegeven map
 cat <<EOF > "$TARGET_DIR/inventory.yaml"
