4b52c3b65bb958d1356438dd66f10a9369a2b8cb
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-13 20:39:19 +0100

    Update cloudflare.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/cloudflare.tf
+++ terraform/V_1/cloudflare.tf
@@ -7,18 +7,3 @@ resource "cloudflare_record" "openvpn_dns" {
   ttl     = 1
   proxied = true  # Als u Cloudflare's proxy-functionaliteit wilt gebruiken, zet dit op true
 }
-
-# Creëer een Cloudflare-tunnel
-resource "cloudflare_tunnel" "openvpn_tunnel" {
-  account_id = var.cloudflare_account_id
-  name       = "openvpn-tunnel"
-}
-
-# Configureer DNS-records zodat ze naar Cloudflare Tunnel verwijzen
-resource "cloudflare_record" "openvpn_dns_tunnel" {
-  zone_id = var.cloudflare_zone_id  
-  name    = "openvpn"
-  type    = "CNAME"
-  content = cloudflare_tunnel.openvpn_tunnel.cname
-  proxied = true
-}
