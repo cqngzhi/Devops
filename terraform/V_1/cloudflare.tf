0a58cc30a0b249a58f6fd4e4a1ba7950aefb1f3b
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-20 14:28:42 +0100

    Delete terraform directory
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- terraform/V_1/cloudflare.tf
+++
@@ -1,9 +0,0 @@
-# Cloudflare DNS record
-resource "cloudflare_record" "openvpn_dns" {
-  zone_id = var.cloudflare_zone_id
-  name    = "openvpn"
-  type    = "A"
-  content = aws_eip.openvpn_ip[0].public_ip  # Het publieke IP van de eerste EC2 instantie (Elastic IP)
-  ttl     = 1
-  proxied = true  # Als u Cloudflare's proxy-functionaliteit wilt gebruiken, zet dit op true
-}
