d34ea7bbdcb2fdcf8b8610d377980ba7d056f894
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-22 13:18:05 +0100

    Create variables.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

---
+++ sociaal_platform/terraform/modules/ec2/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_id" {
+  description = "VPC ID for EC2 instances"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs for EC2 instances"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security group ID for EC2 instances"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key name for EC2 instances"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}
