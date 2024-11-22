f8888cd3ad9ca3141bd387845126c7fe9572a439
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-22 13:27:16 +0100

    Update variables.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/terraform/variables.tf
+++ sociaal_platform/terraform/variables.tf
@@ -34,4 +34,30 @@ variable "security_group_id" {
   description = "Security group ID"
   type        = string
 }
+# AWS Variables
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
+variable "aws_session_token" {
+  description = "AWS Session Token (optional, required for temporary credentials)"
+  type        = string
+  default     = ""
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+}
 
