d58bf6e6d8e1c40af850ae1b9f8e9e74095986a0
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-23 14:35:33 +0100

    Update outputs.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/terraform/modules/ec2/outputs.tf
+++ sociaal_platform/terraform/modules/ec2/outputs.tf
@@ -1,3 +1,11 @@
+output "ec2_public_ip" {
+  value = aws_instance.s_platform_instance.public_ip
+}
+
+output "ec2_private_ip" {
+  value = aws_instance.s_platform_instance.private_ip
+}
+
 output "public_ip" {
   value = aws_eip.s_platform_ip[0].public_ip
 }
