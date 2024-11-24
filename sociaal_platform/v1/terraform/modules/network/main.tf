796d1c06e6bd2a792cfb6116c636a8813a4e9f49
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-25 00:38:28 +0100

    Update main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v1/terraform/modules/network/main.tf
+++ sociaal_platform/v1/terraform/modules/network/main.tf
@@ -30,18 +30,54 @@ resource "aws_subnet" "private" {
 }
 
 # Creëert een beveiligingsgroep
-resource "aws_security_group" "default" {
-  vpc_id = aws_vpc.main.id  # Verwijst naar de VPC voor de beveiligingsgroep
+# Platform Security Group - Beveiliging voor het platform
+resource "aws_security_group" "s_platform_sg" {
+  vpc_id = aws_vpc.s_platform_vpc.id  # Verwijst naar de VPC voor de beveiligingsgroep
+  # Inkomende regels (ingress) voor HTTP-toegang binnen de Kubernetes cluster
   ingress {
-    from_port   = 22  # SSH toegang via poort 22
-    to_port     = 22  # Toegang via poort 22
+    from_port   = 80  # HTTP toegang via poort 80
+    to_port     = 80  # Toegang via poort 80 voor interne HTTP-verkeer
+    protocol    = "tcp"  # TCP protocol
+    cidr_blocks = ["10.0.0.0/16"]  # Alleen toegestaan binnen de VPC netwerk
+  }
+
+  # Inkomende regels (ingress) voor HTTPS-toegang binnen de Kubernetes cluster
+  ingress {
+    from_port   = 443  # HTTPS toegang via poort 443
+    to_port     = 443  # Toegang via poort 443 voor interne HTTPS-verkeer
+    protocol    = "tcp"  # TCP protocol
+    cidr_blocks = ["10.0.0.0/16"]  # Alleen toegestaan binnen de VPC netwerk
+  }
+  ingress {
+    from_port   = 22  # Toegang via poort 22 voor SSH
+    to_port     = 22  # Toegang via poort 22 voor SSH
     protocol    = "tcp"  # TCP protocol
     cidr_blocks = ["0.0.0.0/0"]  # Toegestaan van overal
   }
+
+  ingress {
+    from_port   = 80  # Toegang via poort 80 voor HTTP
+    to_port     = 80  # Toegang via poort 80 voor HTTP
+    protocol    = "tcp"  # TCP protocol
+    cidr_blocks = ["0.0.0.0/0"]  # Toegestaan van overal
+  }
+
+  ingress {
+    from_port   = 443  # Toegang via poort 443 voor HTTPS
+    to_port     = 443  # Toegang via poort 443 voor HTTPS
+    protocol    = "tcp"  # TCP protocol
+    cidr_blocks = ["0.0.0.0/0"]  # Toegestaan van overal
+  }
+
   egress {
     from_port   = 0  # Alle uitgaande poorten
     to_port     = 0  # Alle uitgaande poorten
     protocol    = "-1"  # Alle protocollen
     cidr_blocks = ["0.0.0.0/0"]  # Toegestaan van overal
   }
+
+  tags = {
+    Name = "s_platform_security_group"
+  }
 }
+
