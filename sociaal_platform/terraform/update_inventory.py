c8fd2b07354595cb2a4d0c6ca26813a22bf81e77
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-24 02:55:07 +0100

    Update update_inventory.py
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/terraform/update_inventory.py
+++ sociaal_platform/terraform/update_inventory.py
@@ -18,27 +18,27 @@ def get_terraform_output(output_name):
         print(f"Error fetching {output_name} from Terraform: {e}")
         return None
 
-# 获取每个节点的 IP 地址
-node1_ip = get_terraform_output('public_ip_node1')
-node2_ip = get_terraform_output('public_ip_node2')
-node3_ip = get_terraform_output('public_ip_node3')
+# 获取 Bastion 节点的公网 IP 地址
+bastion_ip = get_terraform_output('public_ip_bastion')
 
 # 检查获取到的 IP 是否有效
-if not node1_ip or not node2_ip or not node3_ip:
-    print("One or more public IPs are empty. Exiting.")
+if not bastion_ip:
+    print("Bastion public IP is empty. Exiting.")
     exit(1)
 
 # 读取现有的 inventory.yaml 文件
 with open(INVENTORY_FILE, 'r') as file:
     inventory = yaml.safe_load(file)
 
-# 更新 YAML 文件中的 IP 地址
-inventory['all']['hosts']['node1']['ansible_host'] = node1_ip
-inventory['all']['hosts']['node2']['ansible_host'] = node2_ip
-inventory['all']['hosts']['node3']['ansible_host'] = node3_ip
+# 更新 YAML 文件中的 master 部分的 IP 为 Bastion 公网 IP
+if 'all' in inventory and 'children' in inventory['all'] and 'master' in inventory['all']['children']:
+    for node_ip in list(inventory['all']['children']['master']['hosts'].keys()):
+        # 将 master 部分的 IP 地址替换为公网 IP 地址
+        inventory['all']['children']['master']['hosts'][bastion_ip] = inventory['all']['children']['master']['hosts'].pop(node_ip)
 
 # 写回更新后的 YAML 到文件
 with open(INVENTORY_FILE, 'w') as file:
     yaml.safe_dump(inventory, file, default_flow_style=False)
 
-print(f"Updated inventory.yaml with IPs: {node1_ip}, {node2_ip}, {node3_ip}")
+print(f"Updated master IPs with Bastion public IP: {bastion_ip}")
+
