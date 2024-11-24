e6960a025c624c9a9ceab13823a1527131b4b7d8
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-24 23:34:16 +0100

    Create main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

---
+++ sociaal_platform/v1/terraform/modules/backup/main.tf
@@ -0,0 +1,23 @@
+# Creëert een back-upplan
+resource "aws_backup_plan" "ec2_backup_plan" {
+  name = "ec2-backup-plan"  # Naam van het back-up plan
+
+  rule {
+    rule_name         = "daily-backup"  # Naam van de back-up regel
+    target_backup_vault_name = "default"  # Doel-backup vault
+    schedule          = var.backup_schedule  # Het back-up schema
+    lifecycle {
+      delete {
+        after = var.backup_retention_days  # Verwijder de back-up na de retentieperiode
+      }
+    }
+  }
+}
+
+# Creëert een back-up selectie
+resource "aws_backup_selection" "ec2_backup_selection" {
+  plan_id          = aws_backup_plan.ec2_backup_plan.id  # Verwijst naar het back-up plan
+  name             = "ec2-backup-selection"  # Naam van de selectie
+  iam_role_arn     = "arn:aws:iam::aws_account_id:role/aws-backup-role"  # IAM rol voor back-up
+  resources = [var.ec2_instance_arn]  # Verwijst naar de EC2 instance ARN
+}
