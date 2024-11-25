20acf8ec83f9208acfc70b457273afb7974da928
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-25 01:19:20 +0100

    Update variables.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v1/terraform/variables.tf
+++ sociaal_platform/v1/terraform/variables.tf
@@ -69,3 +69,45 @@ variable "backup_schedule" {
   type        = string
   default     = "cron(0 12 * * ? *)"  # Standaard back-up schema
 }
+
+# Variabele voor de AWS toegangssleutel
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+# Variabele voor de AWS geheime sleutel
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+# Variabele voor het AWS sessie-token
+variable "aws_session_token" {
+  description = "AWS session token"
+  type        = string
+  sensitive   = true
+}
+
+# Variabele voor de Cloudflare API-token
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token"
+  type        = string
+  sensitive   = true
+}
+
+# Variabele voor de Cloudflare account ID
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID"
+  type        = string
+  sensitive   = true
+}
+
+# Variabele voor de Cloudflare Zone-ID
+variable "cloudflare_zone_id" {
+  description = "Cloudflare zone ID"
+  type        = string
+  sensitive   = true
+}
