2c15f62122c7910208bb7e2f1c537fe6b0952fc4
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-26 23:23:15 +0100

    Update main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v1/terraform/modules/ec2/main.tf
+++ sociaal_platform/v1/terraform/modules/ec2/main.tf
@@ -1,3 +1,6 @@
+data "aws_caller_identity" "current" {} # data.aws_caller_identity.current.account_id
+data "aws_region" "current" {} # data.aws_region.current.name
+
 # Upload de SSH sleutel
 resource "aws_key_pair" "ansible_social_platform" {
   key_name   = "ansible_social_platform"  # Naam van de SSH-sleutel
