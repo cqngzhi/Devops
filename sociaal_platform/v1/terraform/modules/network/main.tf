a722ff823d6cc130cc85485f6529ce4d7cc6d345
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-27 00:35:05 +0100

    Update main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v1/terraform/modules/network/main.tf
+++ sociaal_platform/v1/terraform/modules/network/main.tf
@@ -1,6 +1,6 @@
 # Creëert een VPC
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr  # CIDR-blok voor de VPC en publieke subnetten
+  cidr_block = "10.0.0.0/16"  # CIDR-blok voor de VPC en publieke subnetten
   tags = {
     Name = "main-vpc"  # Naam van de VPC
   }
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 # Creëert publieke subnets
 resource "aws_subnet" "public" {
   count = length([var.vpc_cidr])  # Aantal publieke subnets
-  cidr_block              = "10.0.0.0/16"  # CIDR voor het publieke subnet
+  cidr_block              = var.public_subnet_ids  # CIDR voor het publieke subnet
   vpc_id                  = aws_vpc.main.id  # Verwijst naar de VPC
   availability_zone       = var.availability_zones[count.index]  # Beschikbare zone voor het subnet
   map_public_ip_on_launch = true  # Zorg ervoor dat de instances een publiek IP krijgen
@@ -21,11 +21,11 @@ resource "aws_subnet" "public" {
 # Creëert private subnets
 resource "aws_subnet" "private" {
   count = length(var.private_subnets)  # Aantal private subnets
-  cidr_block        = var.private_subnets[count.index]  # CIDR voor elk subnet
+  cidr_block        = var.private_subnets  # CIDR voor elk subnet
   vpc_id            = aws_vpc.main.id  # Verwijst naar de VPC
-  availability_zone = var.availability_zones[count.index]  # Beschikbare zone voor het subnet
+  availability_zone = var.availability_zones  # Beschikbare zone voor het subnet
   tags = {
-    Name = "private-subnet-${count.index}"  # Naam van het subnet
+    Name = "private-subnet"  # Naam van het subnet
   }
 }
 
