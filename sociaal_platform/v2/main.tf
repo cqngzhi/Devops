986a2187ba8372af4b6c66fb14db09819853c198
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-27 20:23:58 +0100

    Update main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v2/main.tf
+++ sociaal_platform/v2/main.tf
@@ -10,16 +10,21 @@ resource "cloudflare_record" "s_platform_dns" {
 resource "null_resource" "generate_inventory" {
   provisioner "local-exec" {
     command = <<EOT
-mkdir -p ./ansible  # Zorg ervoor dat de map bestaat
-cat <<EOF > ./ansible/inventory.ini
-[master]
-$(terraform output -json elastic_ip | jq -r '.[]') ansible_user=ubuntu ansible_ssh_private_key_file=/home/jiaqi/dev/terraform/SSH/id_rsa
+    mkdir -p ./ansible  # Zorg ervoor dat de map bestaat
+    echo "Elastic IP: $(terraform output -json elastic_ip | jq -r '.[]')"  # Debug
+    echo "Node1 IP: $(terraform output -json node1_private_ip | jq -r '.[]')"  # Debug
+    echo "Node2 IP: $(terraform output -json node2_private_ip | jq -r '.[]')"  # Debug
+    echo "Node3 IP: $(terraform output -json node3_private_ip | jq -r '.[]')"  # Debug
+    
+    cat <<EOF > ./ansible/inventory.ini
+    [master]
+    $(terraform output -json elastic_ip | jq -r '.[]') ansible_user=ubuntu ansible_ssh_private_key_file=/home/jiaqi/dev/terraform/SSH/id_rsa
 
-[nodes]
-$(terraform output -json node1_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /')
-$(terraform output -json node2_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /')
-$(terraform output -json node3_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /')
-EOF
-EOT
+    [nodes]
+    $(terraform output -json node1_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /')
+    $(terraform output -json node2_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /')
+    $(terraform output -json node3_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /')
+    EOF
+    EOT
   }
 }
