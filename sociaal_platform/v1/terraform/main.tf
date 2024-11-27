e0ef3f130e086f318fb03634f8d6e0eacdd2c8a0
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-27 01:42:10 +0100

    Update main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v1/terraform/main.tf
+++ sociaal_platform/v1/terraform/main.tf
@@ -14,8 +14,8 @@ module "ec2" {
   ami               = var.ami  # Het ID van de AMI
   instance_type     = var.instance_type  # EC2 instance type
   key_name          = var.key_name  # De naam van het SSH sleutel
-  private_subnet    = module.network.private_subnet  # IDs van private subnets
-  security_group_id = module.network.security_group_id  # Het ID van de beveiligingsgroep
+  private_subnet    = module.network.aws_subnet.private.id  # IDs van private subnets
+  security_group_id = module.network.aws_security_group.s_platform_sg.id  # Het ID van de beveiligingsgroep
   asg_min_size      = var.asg_min_size  # Minimum grootte van de autoscaling groep
   asg_max_size      = var.asg_max_size  # Maximale grootte van de autoscaling groep
   asg_desired_capacity = var.asg_desired_capacity  # Gewenste capaciteit voor autoscaling groep
