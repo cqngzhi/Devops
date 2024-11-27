532192bbfd4837d274234918e396cd21cd24cedc
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-27 19:04:04 +0100

    Update main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v2/main.tf
+++ sociaal_platform/v2/main.tf
@@ -1,1 +1,15 @@
+resource "null_resource" "generate_inventory" {
+  provisioner "local-exec" {
+    command = <<EOT
+mkdir -p ./ansible  # Zorg ervoor dat de map bestaat
+cat <<EOF > ./ansible/inventory.ini
+[master]
+$(terraform output -json public_ips | jq -r '.[]') ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
+
+[nodes]
+$(terraform output -json private_ips | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /') 
+EOF
+EOT
+  }
+}
 
