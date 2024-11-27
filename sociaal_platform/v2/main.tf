9185e650f9cfe6c94bd876f892ab6c9e1728a4c7
Jiaqi <80153716+cqngzhi@users.noreply.github.com>
2024-11-27 20:02:09 +0100

    Update main.tf
    Signed-off-by: Jiaqi <80153716+cqngzhi@users.noreply.github.com>

--- sociaal_platform/v2/main.tf
+++ sociaal_platform/v2/main.tf
@@ -4,12 +4,13 @@ resource "null_resource" "generate_inventory" {
 mkdir -p ./ansible  # Zorg ervoor dat de map bestaat
 cat <<EOF > ./ansible/inventory.ini
 [master]
-$(terraform output -json public_ips | jq -r '.[]') ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
+$(terraform output -json elastic_ip | jq -r '.[]') ansible_user=ubuntu ansible_ssh_private_key_file=~./SSH/id_rsa
 
 [nodes]
-$(terraform output -json private_ips | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~\/.ssh\/id_rsa /') 
+$(terraform output -json node1_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~./SSH/id_rsa')
+$(terraform output -json node2_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~./SSH/id_rsa')
+$(terraform output -json node3_private_ip | jq -r '.[]' | sed 's/^/ansible_ssh_host=master ansible_user=ubuntu ansible_ssh_private_key_file=~./SSH/id_rsa')
 EOF
 EOT
   }
 }
-
